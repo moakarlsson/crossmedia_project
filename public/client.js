142739add745d3cf79febe361f898167eb7ed561
JosefinBack <josefin.m.mansson@gmail.com>
2026-04-21 15:55:26 +0200

    html-dokument med inloggning

--- public/client.js
+++ public/client.js
@@ -1,5 +1,11 @@
 let timerDisplay = document.getElementById("timerDisplay");
 let stopButton = document.getElementById("stopTimer");
+let logInDiv = document.getElementById("logInDiv");
+let timerDIV = document.getElementById("timerDIV");
+let loginButton = document.getElementById("loginButton");
+
+//ska tas bort sen, endast till för utvecklingen
+let startButton = document.getElementById("startTimer");
 
 //timer
 let startTime = 2 * 60 * 60 + 30 * 60; // 2h 30min = 9000 sek
@@ -30,11 +36,23 @@ function startTimer() {
 
         startTime--;
     }, 1000);
-}
+};
 
-startTimer();
 
 stopButton.addEventListener("click", function () {
     clearInterval(timerInterval);
 });
 
+startButton.addEventListener("click", function () {
+    startTimer();
+});
+
+loginButton.addEventListener("click", function () {
+    spelRegler();
+});
+
+
+function spelRegler() {
+    logInDiv.style.display = "none";
+    timerDIV.style.display = "flex"
+};
