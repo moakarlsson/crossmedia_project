7daacd0d6996cd75968b595cddaa04c7c7ca8086
JosefinBack <josefin.m.mansson@gmail.com>
2026-04-21 12:53:16 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -3,9 +3,10 @@ let stopButton = document.getElementById("stopTimer");
 
 //timer
 let startTime = 2 * 60 * 60 + 30 * 60; // 2h 30min = 9000 sek
+let timerInterval;
 
 function startTimer() {
-    setInterval(function () {
+    timerInterval = setInterval(function () {
         let hours = Math.floor(startTime / 3600);
         let minutes = Math.floor((startTime % 3600) / 60);
         let seconds = startTime % 60;
@@ -20,6 +21,13 @@ function startTimer() {
 
         timerDisplay.textContent = hours + ":" + minutes + ":" + seconds;
 
+        //stoppa vid 0
+        if (startTime <= 0) {
+            clearInterval(timerInterval);
+            timerDisplay.textContent = "0:00:00";
+            return;
+        }
+
         startTime--;
     }, 1000);
 }
@@ -27,6 +35,6 @@ function startTimer() {
 startTimer();
 
 stopButton.addEventListener("click", function () {
-
+    clearInterval(timerInterval);
 });
 
