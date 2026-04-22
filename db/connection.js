681744ea6c296bf6f11f69ae0f28df539730ba58
Moakarlssonns <mo-karlsson@hotmail.com>
2026-04-22 15:49:24 +0200

    refactor: update database connection to use object syntax for pool configuration

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,9 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
-const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
+const pool = mysql.createPool({
+    uri: process.env.MYSQL_PUBLIC_URL
+});
+   
 
 export default pool.promise();
