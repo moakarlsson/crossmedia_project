5ede092c7bb49301b5c14481b24414d662bdaa1e
JosefinBack <josefin.m.mansson@gmail.com>
2026-04-23 08:01:04 +0200

    timer update

--- public/client.js
+++ public/client.js
@@ -8,35 +8,42 @@ let loginButton = document.getElementById("loginButton");
 let startButton = document.getElementById("startTimer");
 
 //timer
-let startTime = 2 * 60 * 60 + 30 * 60; // 2h 30min = 9000 sek
-let timerInterval;
+let endTime;
 
 function startTimer() {
-    timerInterval = setInterval(function () {
-        let hours = Math.floor(startTime / 3600);
-        let minutes = Math.floor((startTime % 3600) / 60);
-        let seconds = startTime % 60;
+    endTime = Date.now() + (2 * 60 * 60 + 30 * 60) * 1000;
+    localStorage.setItem("endTime", endTime);
+    timerInterval = setInterval(updateTimer, 1000);
+}
 
-        if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
+function updateTimer() {
+    let now = Date.now();
+    let timeLeft = Math.floor((endTime - now) / 1000);
 
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
+    if (timeLeft <= 0) {
+        clearInterval(timerInterval);
+        timerDisplay.textContent = "0:00:00";
+        return;
+    }
 
-        timerDisplay.textContent = hours + ":" + minutes + ":" + seconds;
+    let hours = Math.floor(timeLeft / 3600);
+    let minutes = Math.floor((timeLeft % 3600) / 60);
+    let seconds = timeLeft % 60;
 
-        //stoppa vid 0
-        if (startTime <= 0) {
-            clearInterval(timerInterval);
-            timerDisplay.textContent = "0:00:00";
-            return;
-        }
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
 
-        startTime--;
-    }, 1000);
-};
+    timerDisplay.textContent = hours + ":" + minutes + ":" + seconds;
+}
+
+window.addEventListener("load", function () {
+    let savedEndTime = localStorage.getItem("endTime");
+
+    if (savedEndTime) {
+        endTime = Number(savedEndTime);
+        timerInterval = setInterval(updateTimer, 1000);
+    }
+});
 
 
 stopButton.addEventListener("click", function () {
