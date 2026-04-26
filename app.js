48db25ceb08f1d0fa4edaec91ffb43ad9fa82c2f
JosefinBack <josefin.m.mansson@gmail.com>
2026-04-26 13:36:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
+        // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
     }
 }));
 
