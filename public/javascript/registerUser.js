0b3f576fad9b1215176028e27f8e027e139060dd
JosefinBack <josefin.m.mansson@gmail.com>
2026-04-28 15:17:04 +0200

    .

--- public/javascript/registerUser.js
+++ public/javascript/registerUser.js
@@ -1,5 +1,28 @@
 
 let createButton = document.getElementById("create_button");
+let imageInput = document.getElementById("imageUpload");
+let thePic = document.getElementById("thePic");
+
+thePic.addEventListener("click", function () {
+    imageInput.click();
+});
+
+imageInput.addEventListener("change", function () {
+    let file = imageInput.files[0];
+
+    if (file) {
+        let img = document.createElement("img");
+        img.src = URL.createObjectURL(file);
+
+        img.style.width = "100%";
+        img.style.height = "100%";
+        img.style.objectFit = "cover";
+        img.style.borderRadius = "10px";
+
+        thePic.innerHTML = "";
+        thePic.appendChild(img);
+    }
+});
 
 createButton.addEventListener("click", async function () {
     const userName = document.getElementById("username").value;
