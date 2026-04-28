f79b2865314f12753fc86b670ed81d4f1caa4950
JosefinBack <josefin.m.mansson@gmail.com>
2026-04-28 16:01:12 +0200

    .

--- public/javascript/start.js
+++ public/javascript/start.js
@@ -4,3 +4,9 @@ let beginGameButton = document.getElementById("beginGame");
 beginGameButton.addEventListener("click", function () {
     window.location.href = "/html/login.html"
 });
+
+let mailButton = document.getElementById("mail");
+
+mailButton.addEventListener("click", function () {
+    window.location.href = "html/mailpage.html";
+});
