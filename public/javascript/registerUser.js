f02e90fcba52a56db7cd84ea391ab9f756f2c880
JosefinBack <josefin.m.mansson@gmail.com>
2026-05-01 12:26:49 +0200

    ,

--- public/javascript/registerUser.js
+++ public/javascript/registerUser.js
@@ -2,6 +2,14 @@
 let createButton = document.getElementById("create_button");
 let imageInput = document.getElementById("imageUpload");
 let thePic = document.getElementById("thePic");
+let passwordMessage = document.getElementById("passwordMessage");
+let popup = document.getElementById("popup");
+let startaSpelet = document.getElementById("startaSpelet");
+
+startaSpelet.addEventListener("click", function () {
+    window.location.href = "../html/spelregler.html";
+});
+
 
 thePic.addEventListener("click", function () {
     imageInput.click();
@@ -30,9 +38,11 @@ createButton.addEventListener("click", async function () {
     const password_confirm = document.getElementById("password_confirm").value;
 
     if (password === password_confirm) {
-        console.log("Matches")
+        console.log("Matches");
     } else {
         console.error("Passwords are not matching...try again!")
+        passwordMessage.style.display = "block";
+        passwordMessage.textContent = "Lösenorden matchar inte"
         return;
     }
 
@@ -46,7 +56,13 @@ createButton.addEventListener("click", async function () {
 
         console.log(response.status);
         const data = await response.json();
+
+        if (response.status === 200) {
+            popup.classList.remove("hidden");
+        }
+
         console.log(data);
+        passwordMessage.style.display = "none";
     } catch (error) {
         console.log("Fel:", error);
     }
