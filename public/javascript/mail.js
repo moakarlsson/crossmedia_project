4fd6c4b2536ccb5387909fc26a49ecd84313208a
Jenniferjepp <nelliferjj@gmail.com>
2026-05-05 12:50:00 +0200

    input mail

--- public/javascript/mail.js
+++ public/javascript/mail.js
@@ -1,32 +1,53 @@
 const mail1 = document.querySelector("#mail1");
 const mail2 = document.querySelector("#mail2");
+
+const logInMailCOntainerDOM = document.querySelector("#logInMailContainer");
+const mailContainerDOM = document.querySelector("#mailContainer");
 const mailView1 = document.querySelector("#mailView1");
 const mailView2 = document.querySelector("#mailView2");
 
-const bigContainerDOM = document.querySelector("#bigContainer");
+const logInButtonDOM = document.querySelector("button");
+const inputDOM = document.querySelector("input");
+const felmeddelandeDOM = document.querySelector("#felmeddelande");
+
 const arrowIMG1 = document.querySelector("#arrowIMG1");
 const arrowIMG2 = document.querySelector("#arrowIMG2");
 
 
+mailContainerDOM.style.display = "none";
+felmeddelandeDOM.style.display ="none";
+
+logInButtonDOM.addEventListener("click", () => {
+
+    if (inputDOM.value === "lol") {
+        logInMailCOntainerDOM.style.display = "none";
+        mailContainerDOM.style.display = "block";
+    } 
+    
+    else {
+        felmeddelandeDOM.style.display = "block";
+        felmeddelandeDOM.textContent = "fel lösenord";
+    }
+})
+
+
 
 mail1.addEventListener("click", () => {
-    bigContainerDOM.style.display = "none";
+    mailContainerDOM.style.display = "none";
     mailView1.style.display = "block";
 })
 
 mail2.addEventListener("click", () => {
-    bigContainerDOM.style.display = "none";
+    mailContainerDOM.style.display = "none";
     mailView2.style.display = "block";
 })
 
 arrowIMG1.addEventListener("click", () => {
     mailView1.style.display = "none";
-    bigContainerDOM.style.display = "block";
+    mailContainerDOM.style.display = "block";
 })
 
 arrowIMG2.addEventListener("click", () => {
     mailView2.style.display = "none";
-    bigContainerDOM.style.display = "block";
+    mailContainerDOM.style.display = "block";
 })
-
-console.log("test!!!");
