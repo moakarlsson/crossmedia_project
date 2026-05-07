e8476d632d1c9c4d3010fe7d611eaf26b85720e0
Moakarlssonns <mo-karlsson@hotmail.com>
2026-05-07 12:48:37 +0200

    feat: add new news pages and update navigation functionality

---
+++ public/javascript/newspage.js
@@ -0,0 +1,15 @@
+const newsPage_1 = document.getElementById("newspage_1");
+const newsPage_2 = document.getElementById("newspage_2");
+const newsPage_3 = document.getElementById("newspage_3");
+
+newsPage_1.addEventListener("click", function() {
+    window.location.href = "../html/newspage1.html"
+});
+
+newsPage_2.addEventListener("click", function() {
+    window.location.href = "../html/newspage2.html"
+});
+
+newsPage_3.addEventListener("click", function() {
+    window.location.href = "../html/newspage3.html"
+});
