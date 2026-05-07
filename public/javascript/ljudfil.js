e48a4e45859a23862b4157a320dbfafd25e7b9a5
Moakarlssonns <mo-karlsson@hotmail.com>
2026-05-07 21:19:14 +0200

    feat: update next page button visibility and styling for improved user experience

--- public/javascript/ljudfil.js
+++ public/javascript/ljudfil.js
@@ -5,7 +5,6 @@ const next_page_button = document.getElementById("next_page");
 ljudfil.addEventListener("ended", function () {
     next_page_button.style.display = "block";
     setTimeout(() => {
-        next_page_button.style.visibility = "none";
         next_page_button.classList.add("synlig");
     }, 10);
 })
