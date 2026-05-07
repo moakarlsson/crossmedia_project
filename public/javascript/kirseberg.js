ba94c09d3237aa0825c389c1e7d82824585c7607
JosefinBack <josefin.m.mansson@gmail.com>
2026-05-07 14:41:14 +0200

    .

--- public/javascript/kirseberg.js
+++ public/javascript/kirseberg.js
@@ -128,10 +128,25 @@ function clueFive() {
     window.currentStep = "steg12";
 
     let contentDiv = document.createElement("div");
+
     let divInputFält = document.createElement("div");
+    divInputFält.classList.add("divInputFält");
+
+    let endingText = document.createElement("p");
+    endingText.classList.add("endingText");
+    endingText.textContent = "Nu närmar ni er slutet och snart har ni möjlight att stoppa mördaren! Nu ska ni få ange erna misstankar kring hur och när morden begicks."
+
+    let h2 = document.createElement("h2");
+    h2.textContent = "Mordvapnet"
+
     let inputMessage = document.createElement("p");
     inputMessage.style.visibility = "hidden";
+    inputMessage.style.margin = "3px";
+    inputMessage.style.height = "18px";
+
     let hiddenText = document.createElement("p");
+    hiddenText.style.visibility = "hidden";
+    hiddenText.textContent = " = vätecyanid";
 
     let label = document.createElement("label");
     label.textContent = "Mordvapnet: ";
@@ -140,25 +155,199 @@ function clueFive() {
     let inputFält = document.createElement("input");
     inputFält.type = "text";
     inputFält.id = "textinput";
+    inputFält.style.height = "20px";
+    inputFält.style.width = "65px";
 
     let button = document.createElement("button");
     button.textContent = "OK";
 
-    divInputFält.append(label, inputFält);
+    let nextButton = document.createElement("button");
+    nextButton.textContent = "Gå vidare";
+    nextButton.classList.add("buttonPlacementFive");
+
+
+    contentDiv.append(endingText);
+    contentDiv.append(h2);
+    divInputFält.append(label, inputFält, hiddenText);
     contentDiv.append(divInputFält);
     contentDiv.append(inputMessage);
-    spelKirsebergDIV.append(contentDiv);
-
+    contentDiv.append(button);
+    spelKirsebergDIV.append(contentDiv, nextButton);
 
     button.addEventListener("click", function () {
         let answer = inputFält.value.toLowerCase();
         if (answer === "hcn") {
             inputMessage.style.visibility = "visible";
             inputMessage.textContent = "Rätt input!";
+            hiddenText.style.visibility = "visible";
+            nextButton.style.display = "block"
         } else {
             inputMessage.style.visibility = "visible";
             inputMessage.textContent = "Fel input, försök igen!";
         }
     });
+
+    nextButton.addEventListener("click", function () {
+        clueSix();
+    })
 }
-clueFive()
+clueFive();
+
+
+function clueSix() {
+
+    spelKirsebergDIV.innerHTML = "";
+    window.currentStep = "steg13";
+
+    let symbolDiv = document.createElement("div");
+    symbolDiv.classList.add("symbolDiv");
+
+    let nextButton = document.createElement("button");
+    nextButton.textContent = "Gå vidare";
+    nextButton.classList.add("buttonPlacementFive");
+
+    let buttonDivs = document.createElement("div");
+    let buttonSymbol1 = document.createElement("button");
+    buttonSymbol1.textContent = "✖️";
+    let buttonSymbol2 = document.createElement("button");
+    buttonSymbol2.textContent = "⚫";
+    let buttonSymbol3 = document.createElement("button");
+    buttonSymbol3.textContent = "◼️";
+
+    let codeText = document.createElement("p");
+
+    buttonSymbol1.id = "symbolButton";
+    buttonSymbol2.id = "symbolButton";
+    buttonSymbol3.id = "symbolButton";
+
+    buttonDivs.append(buttonSymbol2, buttonSymbol3, buttonSymbol1);
+    symbolDiv.append(buttonDivs);
+
+    let allSmalDivs = document.createElement("div");
+    allSmalDivs.classList.add("allSmalDivs");
+
+    let divSymbol1 = document.createElement("div");
+    let divSymbol2 = document.createElement("div");
+    let divSymbol3 = document.createElement("div");
+    let divSymbol4 = document.createElement("div");
+
+    divSymbol1.classList.add("smallDiv");
+    divSymbol2.classList.add("smallDiv");
+    divSymbol3.classList.add("smallDiv");
+    divSymbol4.classList.add("smallDiv");
+    divSymbol4.textContent = "3"
+
+    allSmalDivs.append(divSymbol1, divSymbol2, divSymbol3, divSymbol4);
+    symbolDiv.append(allSmalDivs, codeText);
+
+    spelKirsebergDIV.append(symbolDiv, nextButton);
+
+
+
+    let correctCode = ["✖️", "⚫", "◼️"];
+    let playerCode = [];
+
+    buttonSymbol1.addEventListener("click", function () {
+
+        playerCode.push("✖️");
+
+        divSymbol1.textContent = playerCode[0] || "";
+        divSymbol2.textContent = playerCode[1] || "";
+        divSymbol3.textContent = playerCode[2] || "";
+
+        if (playerCode.length === 3) {
+            let playerAnswer = playerCode.join("");
+            let correctAnswer = correctCode.join("");
+
+            if (playerAnswer === correctAnswer) {
+                codeText.style.visibility = "visible";
+                codeText.textContent = "Rätt kod!";
+
+                divSymbol1.textContent = "1";
+                divSymbol2.textContent = "9"
+                divSymbol3.textContent = "1"
+
+                nextButton.style.display = "block";
+            }
+            else {
+                codeText.style.visibility = "visible";
+                codeText.textContent = "Fel kod!";
+                playerCode = [];
+
+                divSymbol1.textContent = "";
+                divSymbol2.textContent = "";
+                divSymbol3.textContent = "";
+            }
+        }
+    });
+
+    buttonSymbol2.addEventListener("click", function () {
+
+        playerCode.push("⚫");
+
+        divSymbol1.textContent = playerCode[0] || "";
+        divSymbol2.textContent = playerCode[1] || "";
+        divSymbol3.textContent = playerCode[2] || "";
+
+        if (playerCode.length === 3) {
+            let playerAnswer = playerCode.join("");
+            let correctAnswer = correctCode.join("");
+
+            if (playerAnswer === correctAnswer) {
+                codeText.style.visibility = "visible";
+                codeText.textContent = "Rätt kod!";
+
+                divSymbol1.textContent = "1";
+                divSymbol2.textContent = "9"
+                divSymbol3.textContent = "1"
+
+                nextButton.style.display = "block";
+            }
+            else {
+                codeText.style.visibility = "visible";
+                codeText.textContent = "Fel kod!";
+                playerCode = [];
+
+                divSymbol1.textContent = "";
+                divSymbol2.textContent = "";
+                divSymbol3.textContent = "";
+            }
+        }
+
+    });
+
+    buttonSymbol3.addEventListener("click", function () {
+
+        playerCode.push("◼️");
+
+        divSymbol1.textContent = playerCode[0] || "";
+        divSymbol2.textContent = playerCode[1] || "";
+        divSymbol3.textContent = playerCode[2] || "";
+
+        if (playerCode.length === 3) {
+            let playerAnswer = playerCode.join("");
+            let correctAnswer = correctCode.join("");
+
+            if (playerAnswer === correctAnswer) {
+                codeText.style.visibility = "visible";
+                codeText.textContent = "Rätt kod!";
+
+                divSymbol1.textContent = "1";
+                divSymbol2.textContent = "9"
+                divSymbol3.textContent = "1"
+
+                nextButton.style.display = "block";
+            }
+            else {
+                codeText.style.visibility = "visible";
+                codeText.textContent = "Fel kod!";
+                playerCode = [];
+
+                divSymbol1.textContent = "";
+                divSymbol2.textContent = "";
+                divSymbol3.textContent = "";
+            }
+        }
+
+    });
+}
