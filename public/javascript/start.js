523714c8adec9244c188296844ca6b8f7d8c4fe6
JosefinBack <josefin.m.mansson@gmail.com>
2026-05-07 21:17:24 +0200

    Update start.js

--- public/javascript/start.js
+++ public/javascript/start.js
@@ -7,12 +7,3 @@ beginGameButton.addEventListener("click", function () {
     window.location.href = "/html/spelregler.html"
 });
 
-mailButton.addEventListener("click", function () {
-    window.location.href = "../html/mailpage.html";
-});
-
-let kirsebergButton = document.getElementById("kirseberg");
-
-kirsebergButton.addEventListener("click", function () {
-    window.location.href = "../html/kirseberg.html";
-});
